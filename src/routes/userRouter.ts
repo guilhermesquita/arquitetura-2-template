92efbb26a7913b6e60aa9d391e1459b5de73f20b
Guilherme Mesquita <guilhermer_mesquita@hotmail.com>
2023-12-07 17:13:43 -0400

    create router for users

---
+++ src/routes/userRouter.ts
@@ -0,0 +1,8 @@
+import  express from "express";
+import { UserController } from "../controller/UserController";
+
+export const userRoutes = express.Router();
+const userController = new UserController()
+
+userRoutes.get('/', userController.getUsers)
+userRoutes.get('/', userController.createUser)
